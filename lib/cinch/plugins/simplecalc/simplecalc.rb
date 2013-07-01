7f5dcff11d557c58c94852e9b70b917d1643bb9c
Brian Haberer <bhaberer@gmail.com>
2013-07-01 11:29:10 -0700

    Passing tests

--- lib/cinch/plugins/simplecalc/simplecalc.rb
+++ lib/cinch/plugins/simplecalc/simplecalc.rb
@@ -1,4 +1,5 @@
 require 'calc'
+require 'cinch'
 require 'cinch-cooldown'
 
 module Cinch::Plugins
@@ -12,15 +13,16 @@ module Cinch::Plugins
     match /math (.+)/
 
     def execute(m, problem)
-      begin
-        answer = Calc.evaluate(problem)
+      m.reply calc(problem), true
+    end
 
-        unless answer == problem
-          m.reply "#{Calc.evaluate(problem)}", true
-        end
-      rescue ZeroDivisionError
-        m.reply "Fuck you.", true
-      end
+    private
+
+    def calc(problem)
+      answer = Calc.evaluate(problem)
+      return "#{Calc.evaluate(problem)}" unless answer == problem
+    rescue ZeroDivisionError
+      return "Fuck you."
     end
   end
 end
--- spec/cinch-simplecalc_spec.rb
+++ spec/cinch-simplecalc_spec.rb
@@ -10,13 +10,19 @@ describe Cinch::Plugins::SimpleCalc do
 
   it 'should allow users to perform simple math' do
     msg = make_message(@bot, '!math 2 + 2')
-    get_replies(msg).
-      should == '4'
+    get_replies(msg).last.
+      should == 'test: 4'
   end
 
   it 'should strip all non numeric information from the string' do
     msg = make_message(@bot, '!math cos 2')
-    get_replies(msg).
-      should == '2'
+    get_replies(msg).last.
+      should == 'test: 2'
+  end
+
+  it 'should crack wise if asked to do the impossible' do
+    msg = make_message(@bot, '!math 1 / 0')
+    get_replies(msg).last.
+      should == 'test: Fuck you.'
   end
 end
